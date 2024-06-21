993fc40f214329c02426dd107302100a32c651e2
M Iqbal Zulkarnaen N <m.iqbal.z.n@gmail.com>
2024-06-21 20:10:49 +0700

    add transfer test

--- src/test/java/com/zulkan/ewallet/e2e/ApplicationTest.java
+++ src/test/java/com/zulkan/ewallet/e2e/ApplicationTest.java
@@ -3,11 +3,11 @@ package com.zulkan.ewallet.e2e;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.zulkan.ewallet.dto.request.BalanceTopupRequest;
+import com.zulkan.ewallet.dto.request.TransferRequest;
 import com.zulkan.ewallet.model.User;
 import com.zulkan.ewallet.repository.TransactionRepository;
 import com.zulkan.ewallet.repository.UserRepository;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,7 +19,6 @@ import java.util.Collections;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ApplicationTest {
-
 
     @Autowired
     private TestRestTemplate restTemplate;
@@ -48,7 +47,7 @@ public class ApplicationTest {
 
         firstUser.setToken("secretKey");
         firstUser.setUsername("zulkan");
-        firstUser.setBalance(10000);
+        firstUser.setBalance(100000);
 
         userRepository.save(firstUser);
         userRepository.flush();
@@ -95,11 +94,64 @@ public class ApplicationTest {
 
         var transactions = transactionRepository.findAll();
 
-        Assertions.assertEquals(300000, transactions.get(0).getAmount());
+        Assertions.assertEquals(requestDTO.getAmount(), transactions.get(0).getAmount());
 
         var latestUserData = userRepository.getUserByUsername(firstUser.getUsername());
 
-        Assertions.assertEquals(10000 + 300000, latestUserData.getBalance());
+        Assertions.assertEquals(firstUser.getBalance() + requestDTO.getAmount(), latestUserData.getBalance());
+
+
+    }
+
+    @Test
+    void transferTest() throws InterruptedException {
+
+        var destinationUser = new User();
+        destinationUser.setId(33);
+        destinationUser.setUsername("destinationUser");
+        destinationUser.setBalance(0);
+        destinationUser.setToken("2ndsecret");
+
+        userRepository.save(destinationUser);
+
+        var users = userRepository.findAll();
+
+        Assertions.assertEquals(2, users.size());
+
+        TransferRequest requestDTO = new TransferRequest();
+        requestDTO.setAmount(30000);
+        requestDTO.setToUsername(destinationUser.getUsername());
+
+        setAuth();
+
+        var resp = restTemplate.postForEntity(baseUrl + "/transactions/transfer", requestDTO, String.class );
+
+        System.out.println(resp.getBody());
+
+        Assertions.assertEquals(200, resp.getStatusCodeValue());
+        Assertions.assertTrue(resp.getBody().contains("transfer request received"));
+
+        int ct = 0;
+        while(ct++<30) {
+            if(transactionRepository.count() == 1) {
+                break;
+            }
+            Thread.sleep(1000);
+        }
+
+        Assertions.assertEquals(1, transactionRepository.count());
+
+        var transactions = transactionRepository.findAll();
+
+        Assertions.assertEquals(requestDTO.getAmount(), transactions.get(0).getAmount());
+
+        var latestUserData = userRepository.getUserByUsername(firstUser.getUsername());
+
+        Assertions.assertEquals(firstUser.getBalance()  - requestDTO.getAmount(), latestUserData.getBalance());
+
+
+        var latestDestinationUserData = userRepository.getUserByUsername(destinationUser.getUsername());
+        Assertions.assertEquals(destinationUser.getBalance()  + requestDTO.getAmount(), latestDestinationUserData.getBalance());
 
 
     }
